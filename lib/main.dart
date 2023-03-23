62b7990a5b9e1f451047128a411c34a285bda25e
hansgabriel <hansdaduya@yahoo.com>
2023-03-23 21:46:48 +0800

    revert to old screens

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ridesafe_api/api_endpoints.dart';
 import 'package:ridesafe_api/ridesafe_api.dart';
 import 'package:shca_test/screens/terminal/terminal_screen.dart';
+import 'package:shca_test/screens/username_screen.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:shca_test/models/contacts_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -44,8 +45,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      // home: const UsernameScreen(),
-      home: TerminalScreen(ridesafe: _ridesafe),
+      home: const UsernameScreen(),
+      // home: TerminalScreen(ridesafe: _ridesafe),
     );
   }
 }
